d597f013cb1ffb13028a0cc9a314adf794d0b32c
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-04-07 15:19:57 +0300

    Add preemptive token refresh and timed token refresh for SSO sessions

--- auth/src/main/java/com/alfresco/auth/AuthInterceptor.kt
+++ auth/src/main/java/com/alfresco/auth/AuthInterceptor.kt
@@ -2,7 +2,7 @@ package com.alfresco.auth
 
 import android.content.Context
 import com.alfresco.auth.pkce.PkceAuthService
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import net.openid.appauth.AuthState
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -11,9 +11,13 @@ import org.json.JSONException
 
 class AuthInterceptor(private val context: Context, private val stateString: String, private val config: AuthConfig) : Interceptor {
 
+    private val localScope = CoroutineScope(Dispatchers.IO)
+
     private var listener: Listener? = null
-    private var pkceAuthService: PkceAuthService
+    private val pkceAuthService: PkceAuthService
     private var lastRefresh = 0L
+
+    private var scheduledRefreshJob: Job? = null
 
     init {
         val state = try { AuthState.jsonDeserialize(stateString) } catch (ex: JSONException) { null }
@@ -24,15 +28,17 @@ class AuthInterceptor(private val context: Context, private val stateString: Str
         this.listener = listener
     }
 
-    // TODO: Thread Safe
     override fun intercept(chain: Interceptor.Chain): Response {
-        val state = pkceAuthService.getAuthState() ?: return chain.proceed(chain.request())
+        var state = pkceAuthService.getAuthState() ?: return chain.proceed(chain.request())
+
+        // Preemptive token refresh when close to expiration
+        refreshTokenIfNeeded(state)?.let { state = it  }
 
         var response = chain.proceed(state.accessToken)
 
-        // TODO: how about basic auth?
-        if (response.code == 401) {
-            val newState = refreshToken(state)
+        // When unauthorized try to refresh
+        if (response.code == HTTP_RESPONSE_401_UNAUTHORIZED) {
+            val newState = refreshTokenNow()
 
             if (newState != null) {
                 response.close()
@@ -40,37 +46,80 @@ class AuthInterceptor(private val context: Context, private val stateString: Str
             }
         }
 
-        if (response.code in 400..403) {
-            listener?.onAuthFailure()
+        // If still error notify listener of failure
+        when(response.code) {
+            HTTP_RESPONSE_400_BAD_REQUEST,
+            HTTP_RESPONSE_401_UNAUTHORIZED,
+            HTTP_RESPONSE_403_FORBIDDEN -> listener?.onAuthFailure()
         }
 
         return response
     }
 
-    @Synchronized private fun refreshToken(state: AuthState?): AuthState? {
+    @Synchronized private fun refreshTokenNow(): AuthState? {
+        val state = pkceAuthService.getAuthState() ?: return null
+
+        // Another thread might've refreshed the token already
         val current = System.currentTimeMillis()
-        if (current - lastRefresh < REFRESH_TOKEN_DELAY) {
+        if (current - lastRefresh < REFRESH_THROTTLE_DELAY) {
             return state
         }
 
+        cancelScheduledTokenRefresh()
         lastRefresh = current
-        var newState: AuthState? = null
 
-        runBlocking {
-            with(pkceAuthService.refreshToken()) {
-                onSuccess {
-                    newState = pkceAuthService.getAuthState()
-                    newState?.jsonSerializeString()?.let { state ->
-                        listener?.onAuthStateChange(state)
-                    }
-                }
-                onError {
-                    // do nothing
-                }
-            }
+        val result = runBlocking { runTokenRefresh() }
+
+        if (result != null) {
+            scheduleTokenRefresh(result)
         }
 
-        return newState
+        return result
+    }
+
+    private fun refreshTokenIfNeeded(state: AuthState): AuthState? {
+        val expiration = state.accessTokenExpirationTime ?: return null
+        val delta = expiration - System.currentTimeMillis()
+
+        if (delta < REFRESH_DELTA_BEFORE_EXPIRY) {
+            return refreshTokenNow()
+        } else {
+            scheduleTokenRefresh(state)
+        }
+
+        return null
+    }
+
+    @Synchronized fun scheduleTokenRefresh(state: AuthState) {
+        val expiration = state.accessTokenExpirationTime ?: return
+
+        val delta = expiration - System.currentTimeMillis() - REFRESH_DELTA_BEFORE_EXPIRY
+        if (delta < 0) return
+
+        cancelScheduledTokenRefresh()
+        scheduledRefreshJob = localScope.launch {
+            delay(delta)
+            refreshTokenNow()
+        }
+    }
+
+    private fun cancelScheduledTokenRefresh() {
+        scheduledRefreshJob?.cancel()
+        scheduledRefreshJob = null
+    }
+
+    private suspend fun runTokenRefresh(): AuthState? {
+        val result = pkceAuthService.refreshToken()
+
+        if (result.isSuccess) {
+            val state = pkceAuthService.getAuthState()
+            state?.jsonSerializeString()?.let {
+                listener?.onAuthStateChange(it)
+            }
+            return state
+        }
+
+        return null
     }
 
     private fun Interceptor.Chain.proceed(token: String?): Response {
@@ -83,7 +132,12 @@ class AuthInterceptor(private val context: Context, private val stateString: Str
             .apply { header("Authorization", "Bearer $token") }
 
     companion object {
-        private const val REFRESH_TOKEN_DELAY = 5000L
+        private const val HTTP_RESPONSE_400_BAD_REQUEST = 400
+        private const val HTTP_RESPONSE_401_UNAUTHORIZED = 401
+        private const val HTTP_RESPONSE_403_FORBIDDEN = 403
+
+        private const val REFRESH_THROTTLE_DELAY = 15000L
+        private const val REFRESH_DELTA_BEFORE_EXPIRY = 20000L
     }
 
     interface Listener {
