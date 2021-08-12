c3e1dda404824ab2eb154e516e21f2f15e7df715
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2021-08-12 12:23:17 +0530

    add check to differentiate the version either enterprise or community

--- auth/src/main/java/com/alfresco/auth/ui/AuthenticationActivity.kt
+++ auth/src/main/java/com/alfresco/auth/ui/AuthenticationActivity.kt
@@ -42,6 +42,12 @@ abstract class AuthenticationViewModel : ViewModel() {
     var isReLogin = false
 
     /**
+     * true if the enterprise version otherwise false.
+     */
+
+    var isEnterprise = false
+
+    /**
      * Check which [AuthType] is supported by the [endpoint] based on the provided [authConfig].
      */
     fun checkAuthType(
@@ -51,6 +57,7 @@ abstract class AuthenticationViewModel : ViewModel() {
     ) = viewModelScope.launch {
         discoveryService = DiscoveryService(context, authConfig)
         val authType = withContext(Dispatchers.IO) { discoveryService.getAuthType(endpoint) }
+        isEnterprise = discoveryService.isEnterprise
         onResult(authType)
     }
 
@@ -86,6 +93,7 @@ abstract class AuthenticationViewModel : ViewModel() {
     open fun onPkceAuthCancelled() {}
 
     internal val pkceAuth = PkceAuth()
+
     internal inner class PkceAuth {
         private lateinit var authService: PkceAuthService
 
