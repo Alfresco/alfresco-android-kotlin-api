bdb9b1f30f980cff5ed68ef0d09b3b5e97752c45
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2021-09-29 09:38:12 +0530

    update documentation

--- content/src/main/kotlin/com/alfresco/content/apis/AppConfigApi.kt
+++ content/src/main/kotlin/com/alfresco/content/apis/AppConfigApi.kt
@@ -4,9 +4,14 @@ import com.alfresco.content.models.AppConfigModel
 import retrofit2.http.GET
 import retrofit2.http.Headers
 
+/**
+ * AppConfigApi contains method to fetch config json
+ */
 @JvmSuppressWildcards
 interface AppConfigApi {
-
+    /**
+     * get App config api to fetch the config json for advance search
+     */
     @Headers(
         "Content-Type: application/json"
     )
