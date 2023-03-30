ba15a3b5d4e02faa6c91acdbf51b46cd2e192ca5
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2023-03-30 10:57:44 +0530

    MOBILEAPPS-1738 (#33)
    * added process instance api for listing
    
    * codacy correction

---
+++ process/src/main/kotlin/com/alfresco/process/models/RequestProcessInstances.kt
@@ -0,0 +1,12 @@
+package com.alfresco.process.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+/**
+ * Marked as RequestProcessInstances
+ */
+@JsonClass(generateAdapter = true)
+data class RequestProcessInstances(
+    @Json(name = "sort") @field:Json(name = "sort") var sort: String? = null,
+    @Json(name = "state") @field:Json(name = "state") var state: String? = null
+)
---
+++ process/src/main/kotlin/com/alfresco/process/models/VariablesDataEntry.kt
@@ -0,0 +1,17 @@
+package com.alfresco.process.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * @property name
+ * @property type
+ * @property value
+ */
+
+@JsonClass(generateAdapter = true)
+data class VariablesDataEntry(
+    @Json(name = "name") @field:Json(name = "name") var name: String? = null,
+    @Json(name = "type") @field:Json(name = "type") var type: String? = null,
+    @Json(name = "value") @field:Json(name = "value") var value: String? = null
+)
