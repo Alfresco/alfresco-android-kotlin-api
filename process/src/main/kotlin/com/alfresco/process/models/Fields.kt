7eab0b477002bbd27ddff62651331cbaa97ba5c3
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2024-01-16 10:34:54 +0530

    added dateformat params (#63)

--- process/src/main/kotlin/com/alfresco/process/models/Fields.kt
+++ process/src/main/kotlin/com/alfresco/process/models/Fields.kt
@@ -38,6 +38,7 @@ data class Fields(
     @Json(name = "enableFractions") @field:Json(name = "enableFractions") var enableFractions: Boolean? = null,
     @Json(name = "enablePeriodSeparator") @field:Json(name = "enablePeriodSeparator") var enablePeriodSeparator: Boolean? = null,
     @Json(name = "currency") @field:Json(name = "currency") var currency: String? = null,
+    @Json(name = "dateDisplayFormat") @field:Json(name = "dateDisplayFormat") var dateDisplayFormat: String? = null,
     @Json(name = "params") @field:Json(name = "params") var params: FieldParams? = null,
     @Json(name = "fields") @field:Json(name = "fields") var fields: Map<String, Any>? = null
 ) {
