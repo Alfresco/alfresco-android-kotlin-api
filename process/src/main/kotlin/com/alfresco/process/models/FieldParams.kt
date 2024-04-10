f764c1a906cbc9164cbd6dccd33be10ad3f45eaf
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2024-04-10 16:06:11 +0530

    added params

--- process/src/main/kotlin/com/alfresco/process/models/FieldParams.kt
+++ process/src/main/kotlin/com/alfresco/process/models/FieldParams.kt
@@ -7,10 +7,14 @@ import com.squareup.moshi.JsonClass
  * @property fractionLength
  * @property multiple
  * @property fileSource
+ * @property field
+ * @property dateDisplayFormat
  */
 @JsonClass(generateAdapter = true)
 data class FieldParams(
     @Json(name = "fractionLength") @field:Json(name = "fractionLength") var fractionLength: Int = 0,
     @Json(name = "multiple") @field:Json(name = "multiple") var multiple: Boolean? = null,
     @Json(name = "fileSource") @field:Json(name = "fileSource") var fileSource: FieldSource? = null,
+    @Json(name = "field") @field:Json(name = "field") var field: FieldSource? = null,
+    @Json(name = "dateDisplayFormat") @field:Json(name = "dateDisplayFormat") var dateDisplayFormat: FieldSource? = null,
 )
