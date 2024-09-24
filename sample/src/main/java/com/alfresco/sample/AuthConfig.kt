ddbba13de65d0ed5a4237ec0f25e0efcd143feef
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2024-09-24 19:07:31 +0530

    updated versions

--- sample/src/main/java/com/alfresco/sample/AuthConfig.kt
+++ sample/src/main/java/com/alfresco/sample/AuthConfig.kt
@@ -6,8 +6,8 @@ val AuthConfig.Companion.defaultConfig: AuthConfig
     get() = AuthConfig(
         https = true,
         port = "443",
-        clientId = "zTSfC3VEcrguvP5CKIgNIBCOTliawIKg",
-        realm = "",
-        redirectUrl = "demo://dev-ps-alfresco.auth0.com/android/com.alfresco.sample/callback",
+        clientId = "alfresco-android-acs-app",
+        realm = "alfresco",
+        redirectUrl = "androidacsapp://aims/auth",
         contentServicePath = "alfresco"
     )
